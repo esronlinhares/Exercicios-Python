43a5bbe1a1b17ad9d4dcd1be9ba26088a3f5130e
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-11 17:18:40 -0300

    Update ex100 (Funções Sortear e Somar).py

--- ex100 (Funções Sortear e Somar).py
+++ ex100 (Funções Sortear e Somar).py
@@ -1,15 +1,20 @@
 from random import randint
+
 def sorteia(l):
     for c in range(0, 5):
         n = randint(1, 5)
         l.append(n)
         print(n, end=' ')
+        
+        
 def somaPar(l):
     s = 0
     for v in l:
         if v % 2 == 0:
             s += v
     print(f'A soma dos valores pares de {l} é {s}.')
+    
+    
 numeros = []
 sorteia(numeros)
 print()
