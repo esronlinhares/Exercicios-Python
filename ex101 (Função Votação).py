367853724d7f7369d7eb1918bda6829a8aa4a389
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-11 17:18:09 -0300

    Update ex101 (Função Votação).py

--- ex101 (Função Votação).py
+++ ex101 (Função Votação).py
@@ -8,5 +8,7 @@ def voto(ano):
         return f'Com {idade} anos: VOTO OPCIONAL.'
     else:
         return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+    
+    
 n = int(input('Em que ano você nasceu? '))
 print(voto(n))
