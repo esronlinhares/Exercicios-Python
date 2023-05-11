4cfa0ab4c2e0abaa26ee97eccd1a8e7da5f67d8d
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-11 17:17:36 -0300

    Update ex103 (Função FIca Jogador).py

--- ex103 (Função FIca Jogador).py
+++ ex103 (Função FIca Jogador).py
@@ -1,5 +1,7 @@
 def ficha(j='', gol=0):
     print(f'O jogador {j} fez {gol} gols no campeonato.')
+    
+    
 n = str(input('Nome do jogador: '))
 g = str(input('Número de gols: '))
 if g.isnumeric():
