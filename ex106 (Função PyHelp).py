d76b7a39a801add26fa13a5da819bc5725118857
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-11 17:15:39 -0300

    Update ex106 (Função PyHelp).py

--- ex106 (Função PyHelp).py
+++ ex106 (Função PyHelp).py
@@ -1,11 +1,15 @@
 def ajuda(com):
     titulo(f'Acessando o manual do comando {com}')
     help(com)
+    
+    
 def titulo(msg):
     tam = len(msg) + 4
     print('~'*tam)
     print(f'  {msg}')
     print('~'*tam)
+    
+    
 comando = ''
 while True:
     titulo('Sistema de Ajuda PyHelp')
