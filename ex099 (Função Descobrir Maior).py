0b7302bb5572c0f59cdbe63f340d68f86fe8c5fe
Esron <112833876+esronlinhares@users.noreply.github.com>
2023-05-11 17:19:26 -0300

    Update ex099 (Função Descobrir Maior).py

--- ex099 (Função Descobrir Maior).py
+++ ex099 (Função Descobrir Maior).py
@@ -2,4 +2,6 @@ def maior(*v):
     print(f'Analizando: {v}.')
     print(f'Foram informados {len(v)} valores ao todo.')
     print(f'O maior valor informado foi {max(v)}.')
+    
+    
 maior(2, 3, 4)
